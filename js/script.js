7adf6756d8e6bebced8f7e6ffee8fc0b98fcbd07
jamesBoder <james.macean08@gmail.com>
2024-12-23 19:33:01 -0500

    start

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Javascript
+document.addEventListener('DOMContentLoaded', function() {
+  let heading = document.getElementById('myHeading'); 
+  
+  heading.addEventListener('click', function() {
+    heading.style.color = 'red';
+  });
+
+});
